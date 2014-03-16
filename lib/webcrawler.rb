aed6d90b39996b26e9ce75d24c2d643ffa43c16e
Patricio Cano <suprnova32@gmail.com>
2014-03-16 21:05:55 +0100

    Adding test and counters

--- lib/webcrawler.rb
+++ lib/webcrawler.rb
@@ -3,8 +3,11 @@ require 'anemone'
 
 module Webcrawler
   class Spider
+    attr_accessor :total_js, :total_css
     def initialize(url)
       @url = url
+      @total_css = []
+      @total_js = []
     end
 
     def run
@@ -12,7 +15,11 @@ module Webcrawler
         anemone.on_every_page do |page|
           css = page.doc.css('link')
           css.each do |c|
-            p css.attributes['href'].value
+            @total_css << c.attributes['href'].value unless c.attributes['href'].nil?
+          end
+          scripts = page.doc.css('script').select{ |s| !s.attributes['src'].nil? }
+          scripts.each do |s|
+            @total_js << s.attributes['src'].value
           end
         end
       end
---
+++ spec/webcrawler_spec.rb
@@ -0,0 +1,16 @@
+require 'webcrawler'
+
+describe Webcrawler do
+  describe 'Spider' do
+    before do
+      @crawler = Webcrawler::Spider.new('https://freedomvpn.info')
+      @crawler.run
+    end
+    it 'should count the number of css files' do
+      @crawler.total_css.size.should eq 26
+    end
+    it 'should count the number of js files' do
+      @crawler.total_js.size.should eq 13
+    end
+  end
+end
